dc1eabf6ba84478ed5df4975d5c64a80792da867
Rakan-213 <66526967+Rakan-213@users.noreply.github.com>
2020-06-13 19:16:04 +0300

    تم حل التمرين

---
+++ CW-1-راكان مزيد المطيري-213.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "Rakan"
+var hobby1 = "swimming"
+var hobby2 = "biking"
+var age = 17
+var height = 1.65
+
+print("my name is \(name)")
+print("hi my name is \(name) \(hobby1) \(hobby2) \(age) \(height)")
