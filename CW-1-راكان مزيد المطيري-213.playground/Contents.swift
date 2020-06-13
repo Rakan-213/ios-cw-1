24c47368d172ff1ae80044bbeefe25f0f8b3fa16
Rakan-213 <66526967+Rakan-213@users.noreply.github.com>
2020-06-13 19:57:07 +0300

    done

--- CW-1-راكان مزيد المطيري-213.playground/Contents.swift
+++ CW-1-راكان مزيد المطيري-213.playground/Contents.swift
@@ -10,3 +10,18 @@ var height = 1.65
 
 print("my name is \(name)")
 print("hi my name is \(name) \(hobby1) \(hobby2) \(age) \(height)")
+
+let friendName = "nasser"
+var friendAge = 18
+var friendHeight = 1.70
+let friendHobby1 = "horse rider"
+let friendHobby2 = "climbing"
+var age_difference = friendAge - age
+ 
+age > friendAge
+age < friendAge
+age >= friendAge
+age <= friendAge
+age == friendAge
+age != friendAge
+
